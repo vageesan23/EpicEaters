4f90793320155c031923fda876a86ffee4d8a01f
vageesan23 <60152176+vageesan23@users.noreply.github.com>
2021-12-03 00:38:39 +0530

    added chatbot file

---
+++ iphone/parts/chatbot.php
@@ -0,0 +1,5 @@
+<div id="messenger-button" class="messenger-button-shown" style="width: 58px; height: 58px;">
+    <div class="messenger-button-icon">
+        <img alt="Smooch Messenger Button" src="https://media.smooch.io/5bbaff0c89aae20022c23a36/icons/02a198808025ea82342b9689da348339.jpg">
+    </div>
+</div>
