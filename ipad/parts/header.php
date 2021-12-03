74562a3f2737fbfd4dbd7a6781233ece172d805b
unknown <shangeeran@gmail.com>
2021-12-04 03:31:37 +0530

    changes icons

--- ipad/parts/header.php
+++ ipad/parts/header.php
@@ -4,7 +4,7 @@
 <div data-role="header" data-position="fixed"
      class="header">
     <a data-ajax="false" href="#sideMenu" class="menu-icon"><img alt="page_icon"
-                                                                 src="../../common/assets/images/icons/menu.png"
+                                                                 src="../../common/assets/images/icons/menubar.png"
                                                                  width="50"></a>
-    <img src="../images/logo.png" class="logo" width="127" height="56">
+    <img src="../images/logo2.png" class="logo" width="120" height="55">
 </div>
