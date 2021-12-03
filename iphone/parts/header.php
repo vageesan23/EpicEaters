88bba746c0ce9e1a526ede67ba08a2ea7ad7c0c6
unknown <shangeeran@gmail.com>
2021-12-04 04:19:48 +0530

    updated header

--- iphone/parts/header.php
+++ iphone/parts/header.php
@@ -5,7 +5,7 @@
      class="header"
      style="background: #EF005A !important;">
     <a data-ajax="false" href="#sideMenu" class="menu-icon" style="background: #EF005A !important;"><img alt="page_icon"
-                                                                 src="../../common/assets/images/icons/menu.png"
+                                                                 src="../../common/assets/images/icons/menubar.png"
                                                                  width="50"></a>
-    <img src="../images/logo.png" class="logo" width="127" height="56">
+    <img src="../images/logo2.png" class="logo" width="127" height="56">
 </div>
