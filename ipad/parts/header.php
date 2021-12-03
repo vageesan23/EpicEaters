7f2f5d511c213f59de1dc05c8e68ebef16932f4b
unknown <shangeeran@gmail.com>
2021-12-04 03:52:14 +0530

    updated header

--- ipad/parts/header.php
+++ ipad/parts/header.php
@@ -2,16 +2,9 @@
 <?php include "sideMenu.php" ?>
 <!-- header logo-->
 <div data-role="header" data-position="fixed"
-
-     class="header">
-    <a data-ajax="false" href="#sideMenu" class="menu-icon"><img alt="page_icon"
+     class="header" style="background: #EF005A !important;">
+    <a style="background: #EF005A !important;" data-ajax="false" href="#sideMenu" class="menu-icon"><img alt="page_icon"
                                                                  src="../../common/assets/images/icons/menubar.png"
-
-     class="header"
-     style="background: #EF005A !important;">
-    <a data-ajax="false" href="#sideMenu" class="menu-icon" style="background: #EF005A !important;"><img alt="page_icon"
-                                                                 src="../../common/assets/images/icons/menu.png"
-
                                                                  width="50"></a>
     <img src="../images/logo2.png" class="logo" width="120" height="55">
 </div>
