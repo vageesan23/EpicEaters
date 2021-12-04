29ecaa2aa1b08914aa4272083343082ca856495e
unknown <shangeeran@gmail.com>
2021-12-05 01:20:39 +0530

    updated header

--- ipad/parts/header.php
+++ ipad/parts/header.php
@@ -1,9 +1,21 @@
+<style>
+     @media (max-width: 768px) {
+        .logo {
+            margin-left: -530px !important
+        }
+    }
+    .logo{
+        margin-left: -790px;
+    }
+</style>
+
 <!-- side menu Bar-->
 <?php include "sideMenu.php" ?>
 <!-- header logo-->
 <div data-role="header" data-position="fixed"
-     class="header" style="background: #EF005A !important;">
+     class="header" 
+     style="background: #EF005A !important;">
     <a style="background: #EF005A !important;" data-ajax="false" href="#sideMenu" class="menu-icon">
     <img alt="page_icon" src="../../common/assets/images/icons/menubar.png" width="50" height="50"></a>
-    <img src="../images/logo2.png" style="margin-left: -550px" class="logo" width="120" height="55">
+    <img src="../images/logo2.png" class="logo" width="120" height="55">
 </div>
--- iphone/parts/header.php
+++ iphone/parts/header.php
@@ -1,4 +1,15 @@
-<!-- side menu-->
+<style>
+     @media (max-width: 380px) {
+        .logo {
+            margin-left: -150px !important
+        }
+    }
+    .logo{
+        margin-left: -580px;
+    }
+</style>
+
+<!-- side menu Bar-->
 <?php include "sideMenu.php" ?>
 <!-- header logo-->
 <div data-role="header" data-position="fixed"
@@ -6,5 +17,5 @@
      style="background: #EF005A !important;">
     <a data-ajax="false" href="#sideMenu" class="menu-icon" style="background: #EF005A !important;">
     <img alt="page_icon" src="../../common/assets/images/icons/menubar.png" width="50" height="50"></a>
-    <img src="../images/logo2.png" style="margin-left: -155px" class="logo" width="120" height="55">
+    <img src="../images/logo2.png" class="logo" width="120" height="55">
 </div>
