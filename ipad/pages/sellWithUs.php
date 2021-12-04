50e97a6f65a6e503bbb304a4cb3bea1fc14d75a4
vageesan23 <60152176+vageesan23@users.noreply.github.com>
2021-12-04 20:44:12 +0530

    Updated files

--- ipad/pages/sellWithUs.php
+++ ipad/pages/sellWithUs.php
@@ -3,8 +3,7 @@
 <div data-role="page" id="sellWithUs" style='background-image: url("../../common/assets/images/christmas.jpg"); background-size: cover;'>
     <?php include '../parts/header.php' ?>
 
-    <div role="main" class="ui-content"
-         style='background-image: url("../common/assets/images/christmas.jpg"); background-size: cover;'>
+    <div role="main" class="ui-content" style='background-image: url("../common/assets/images/christmas.jpg"); background-size: cover;'>
 
         <div class="back-box" style="padding: 50px 20px; background-color: rgba(0, 0, 0, 0.7);">
             <div class="header-title" style="color: white">
@@ -23,16 +22,14 @@
                     <div class="ui-block-c" style="width: 40%">
                         <input type="text" name="lastName" id="lastName" value="" placeholder="Last Name">
                         <input type="text" name="nic" id="nic" value="" placeholder="NIC">
-                        <br/><br/>
-                        <input style="margin-top: -5px" type="password" name="rePassword" id="rePassword" value=""
-                               placeholder="Re-type Password">
+                        <br /><br />
+                        <input style="margin-top: -5px" type="password" name="rePassword" id="rePassword" value="" placeholder="Re-type Password">
                     </div>
                 </fieldset><!-- /grid-a -->
 
                 <button style="opacity: 1; width: 205px; margin: 25px auto auto auto">Sign Up</button>
                 <center>
-                    <a data-ajax="false" href="logIn.php"
-                       style="font-weight: 100; color: white; font-family: 'Poppins', sans-serif; font-size: 12px;">
+                    <a data-ajax="false" href="logIn.php" style="font-weight: 100; color: white; font-family: 'Poppins', sans-serif; font-size: 12px;">
                         Already have an account?
                     </a>
                 </center>
