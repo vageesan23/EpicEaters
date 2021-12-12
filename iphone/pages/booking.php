e9c50d7ca3f5f39ababed77e34e6fc91a3bb5a98
vageesan23 <60152176+vageesan23@users.noreply.github.com>
2021-12-12 17:17:20 +0530

    Booking forms completed for Iphone

--- iphone/pages/booking.php
+++ iphone/pages/booking.php
@@ -1,55 +1,117 @@
 <?php include '../parts/head.php' ?>
-<script src="https://js.stripe.com/v3/"></script>
-<!-- This is booking page-->
-<div data-role="page" id="booking" style='background-image: url("../../common/assets/images/booking_back.jpg");
-     background-size: cover; background-position: bottom;'>
+<!-- This is sell with us page-->
+
+<style>
+    @media (max-width: 376px) {
+        .header-title {
+            margin-left: 80px !important;
+            margin-bottom: 25px !important;
+            margin-top: -17px !important;
+        }
+
+        .head-icon {
+            position: relative;
+            right: 215px !important;
+            bottom: -6px !important;
+        }
+    }
+
+    .header-title {
+        margin-left: 290px;
+        margin-bottom: 25px !important;
+        margin-top: -17px;
+    }
+
+    .head-icon {
+        position: relative;
+        right: 215px !important;
+        bottom: -6px !important;
+    }
+
+    .dropdown-border {
+        position: relative;
+        bottom: 3px;
+        font-weight: normal;
+    }
+
+    .ui-select>.ui-btn {
+        background-color: white !important;
+    }
+
+    .ui-icon-carat-d {
+        border-radius: 10px !important;
+        max-height: 2.4em !important;
+    }
+
+    .ui-checkbox {
+        position: relative;
+        top: 7px;
+        right: 1px;
+    }
+</style>
+
+<div data-role="page" id="sellWithUs" style='background-image: url("../../common/assets/images/bg-1.png"); background-size: cover;'>
     <?php include '../parts/header.php' ?>
 
-    <div role="main" class="ui-content">
+    <div role="main" class="ui-content" style='background-image: url("../common/assets/images/christmas.png"); background-size: cover;'>
 
-        <div class="back-box" style="padding: 50px 20px; background-color: rgba(0, 0, 0, 0.7)">
-            <div style="color: white;" class="header-title">
-                <h3>Booking</h3>
-                <img alt="page_icon" src="../../common/assets/images/icons/booking.png" height="35" width="35">
+
+        <div style="padding: 50px 20px; background-color: rgba(0, 0, 0, 0.7); border-radius:20px">
+
+            <div class="header-title" style="color: white">
+                <span class="iconify head-icon" alt="page_icon" data-icon="tabler:brand-booking" data-width="45" data-height="45"></span>
+                <h3>Book Course</h3>
             </div>
-            <form id="booking-form"
-                  action="#"
-                  method="POST">
-                <input type="text" name="firstName" id="firstName" value="" placeholder="Name" required>
-                <select name="select-native-1" id="location" required>
-                    <option value="1" disabled hidden selected>Select Location</option>
-                    <option value="2">Nugegoda</option>
-                    <option value="3">Kurunegala</option>
-                    <option value="4">Kandy</option>
+            <form action="https://www.geeksforgeeks.org" target="_blank">
+                <p class="p-text" style="color:#fff; margin-bottom:15px; margin-left:-8px">*Personal Details</p>
+
+                <input type="text" name="firstName" id="firstName" value="" placeholder="First Name" required>
+                <input type="text" name="lastName" id="lastName" value="" placeholder="Last Name" required>
+                <input type="text" name="tel" id="tel" value="" placeholder="Mobile No" required>
+                <input type="text" name="nic" id="nic" value="" placeholder="NIC" required>
+                <input type="text" name="username" id="username" value="" placeholder="Username" required>
+                <input type="text" name="address" id="address" value="" placeholder="Address" required>
+                <input type="text" name="address" id="address" value="" placeholder="Zip Code" required>
+                <br>
+                <p class="p-text" style="color:#fff; margin-bottom:15px; margin-left:-8px">*Personal Details</p>
+
+                <select class="dropdown-border" name="select-native-1" id="location" required>
+                    <option value="1" disabled hidden selected>Select Membership</option>
+                    <option value="2">Annual Membership ( LKR 25,000 )</option>
+                    <option value="3">Monthly Membership ( LKR 6,000 )</option>
                 </select>
-                <select name="select-native-1" id="timeSlot" required>
-                    <option value="1" disabled hidden selected>Select Time Slot</option>
-                    <option value="2">15:00 p.m</option>
-                    <option value="3">16:00 p.m</option>
-                    <option value="4">17:00 p.m</option>
+                <select class="dropdown-border" name="select-native-1" id="session" required>
+                    <option value="1" disabled hidden selected>Select Course</option>
+                    <option value="2">Professional Cookery</option>
+                    <option value="3">Baking Breads and Rolls</option>
+                    <option value="4">Kitchen Skills</option>
+                    <option value="5">International Cuisine</option>
+                    <option value="6">Garde Manger</option>
                 </select>
-                <select name="select-native-1" id="session" required>
-                    <option value="1" disabled hidden selected>Select Session</option>
-                    <option value="2">Hand Craft</option>
-                    <option value="3">Wooden Craft</option>
-                </select>
+
+                <br>
+                <p class="p-text" style="color:#fff; margin-bottom:15px; margin-left:-8px; font-size:13px;">Note: By clicking the "Place Booking" button you are submiting your online application and will be re-directed to a form to submit your payment. Once we recieve your payment, we will contact you about your Membership.</p>
+                <p class="p-text" style="color:#fff; margin-left:-13px; font-size:13px;"><input type="checkbox" name="checkbox-0" required>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Agree to the Terms & Conditions</p>
+
+                <button style="opacity: 1; margin-top:30px;">Place Booking</button>
                 <center>
-                    <span style="color:#fff;">Note: The payment of LKR 2500/= need to paid for these session.</span>
+                    <a data-ajax="false" href="logIn.php" style="font-weight: 100; color: white; font-family: 'Poppins', sans-serif; font-size: 12px; text-decoration:none;">
+                        Already have an account ?
+                    </a>
                 </center>
-                <button style="opacity: 1">Place Booking</button>
             </form>
         </div>
-
     </div>
-<?php include '../parts/bottomNavbar.php' ?>
+    <?php include '../parts/chatbot.php' ?>
+    <?php include '../parts/bottomNavbar.php' ?>
     <?php include '../parts/footer.php' ?>
 </div>
-</body>
+</div>
 <script>
-    window.addEventListener("DOMContentLoaded", function () {
+    window.addEventListener("DOMContentLoaded", function() {
         var form = document.getElementById("booking-form");
 
-        form.addEventListener("submit", function (ev) {
+        form.addEventListener("submit", function(ev) {
             ev.preventDefault();
             // Create an instance of the Stripe object with API key
             const stripe = Stripe("<?php echo $GLOBALS['stripe_key'] ?>");
@@ -58,16 +120,20 @@
             let timeSlot = $('#timeSlot').find(":selected").text();
             let session = $('#session').find(":selected").text();
             fetch("../../common/functions/checkout.php?cat=booking&origin=<?php echo $origin ?? 'iPhone' ?>", {
-                method: "POST",
-                body: JSON.stringify({name: session + " " + timeSlot + " " + location}),
-            })
-                .then(function (response) {
+                    method: "POST",
+                    body: JSON.stringify({
+                        name: session + " " + timeSlot + " " + location
+                    }),
+                })
+                .then(function(response) {
                     return response.json();
                 })
-                .then(function (session) {
-                    return stripe.redirectToCheckout({sessionId: session.id});
+                .then(function(session) {
+                    return stripe.redirectToCheckout({
+                        sessionId: session.id
+                    });
                 })
-                .then(function (result) {
+                .then(function(result) {
                     // If redirectToCheckout fails due to a browser or network
                     // error, you should display the localized error message to your
                     // customer using error.message.
@@ -75,10 +141,9 @@
                         alert(result.error.message);
                     }
                 })
-                .catch(function (error) {
+                .catch(function(error) {
                     console.error("Error:", error);
                 });
         });
     })
 </script>
-</html>
