4a44582e4a673b01b8bf8740da0a867238bba8f1
unknown <shangar.shan33@gmail.com>
2021-12-12 23:58:29 +0530

    Login-page-half

--- ipad/pages/logIn.php
+++ ipad/pages/logIn.php
@@ -1,31 +1,30 @@
-<?php include '../parts/head.php' ?>
+<!-- <?php include '../parts/head.php' ?> -->
 <!-- This is login page-->
 <div data-role="page" id="login"
      style='background-color: #fff; overflow: hidden;'>
-    <!-- <?php include '../parts/header.php' ?> -->
 
     <div role="main" style="display: flex; justify-content: center; margin-bottom: 0px" class="ui-content">
 
 
         <div class="back-box">
             <div class="header-title" style="color: white">
-                <img alt="page_icon" src="../../common/assets/images/icons/login.png" height="35" width="35">
-                <h3 style="color: #000;">Login</h3>
+                <img alt="page_icon" src="../../common/assets/images/icons/login.png" height="35" width="35" style="display: inline-flex;">
+                <h3 style="color: #000; display: inline-flex;">Login</h3>
             </div>
             <form style="color: #1797F3; height: 750px; width: 600px">
                 
-                <img src="../../ipad/images/logo(footer).png" alt="login-img" style="height: 300px; display: block; margin: auto; background-color: #000">
-                <input type="text" name="username" id="username" value="" placeholder="Username/ E-mail" style="box-sizing: border-box; border: none; border-bottom: 2px solid #000; border-radius: 0px; text-align: center; margin-top: 50px; margin-bottom: 50px;">
-                <input type="password" name="password" id="password" value="" placeholder="Password" style="box-sizing: border-box; border: none; border-bottom: 2px solid #000; border-radius: 0px; text-align: center; margin-bottom: 50px;">
+                <img src="../../ipad/images/logo(footer).png" alt="login-img" style="height: 300px; display: block; margin: auto; background-color: #000;">
+                <input type="text" name="username" id="username" value="" placeholder="Username/ E-mail" style="box-sizing: border-box; border: none; border-bottom: 2px solid #000; border-radius: 0px; text-align: center; margin-top: 50px; margin-bottom: 50px; background-color: transparent;">
+                <input type="password" name="password" id="password" value="" placeholder="Password" style="box-sizing: border-box; border: none; border-bottom: 2px solid #000; border-radius: 0px; text-align: center; margin-bottom: 10px;">
                 <input type="checkbox" value="rememberMe" id="rememberMe">
-                <label for="rememberMe">Remember Me</label>
+                <label for="rememberMe" style="color: #000; font-size: 15px;">Remember Me</label>
 
                 <a href="home.php">
-                    <button style="opacity: 1; width: 205px; margin: 25px auto auto auto;">Sign In</button>
+                    <button style="opacity: 1; width: 205px; margin: 25px auto auto auto; text-decoration:none;">Sign In</button>
                 </a>
                 <center>
                     <a data-ajax="false" href="register.php"
-                       style="font-weight: 100; color: #000; font-family: 'Poppins', sans-serif; font-size: 12px;">
+                       style="font-weight: 100; color: #000; font-family: 'Poppins', sans-serif; font-size: 12px; text-decoration: none;">
                         Don't have an account?
                     </a>
                 </center>
