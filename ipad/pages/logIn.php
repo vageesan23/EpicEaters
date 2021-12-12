720f05a5ad7e72d8a53c4c3c64758d1b77e09aef
unknown <shangar.shan33@gmail.com>
2021-12-12 22:50:39 +0530

    Login-Page half done

--- ipad/pages/logIn.php
+++ ipad/pages/logIn.php
@@ -1,29 +1,31 @@
 <?php include '../parts/head.php' ?>
 <!-- This is login page-->
 <div data-role="page" id="login"
-     style='background-image: url("../../common/assets/images/baskets.jpg"); background-size: cover;'>
-    <?php include '../parts/header.php' ?>
+     style='background-color: #fff; overflow: hidden;'>
+    <!-- <?php include '../parts/header.php' ?> -->
 
-    <div role="main" style="display: flex; justify-content: center;" class="ui-content">
+    <div role="main" style="display: flex; justify-content: center; margin-bottom: 0px" class="ui-content">
 
 
-        <div class="back-box"
-             style="padding: 50px 20px; width: 600px; background-color: rgba(0, 0, 0, 0.7); margin-top: 100px;">
+        <div class="back-box">
             <div class="header-title" style="color: white">
-                <h3>Login</h3>
                 <img alt="page_icon" src="../../common/assets/images/icons/login.png" height="35" width="35">
+                <h3 style="color: #000;">Login</h3>
             </div>
-            <form style="color: #1797F3;">
-
-                <input type="text" name="username" id="username" value="" placeholder="Username">
-                <input type="password" name="password" id="password" value="" placeholder="Password">
+            <form style="color: #1797F3; height: 750px; width: 600px">
+                
+                <img src="../../ipad/images/logo(footer).png" alt="login-img" style="height: 300px; display: block; margin: auto; background-color: #000">
+                <input type="text" name="username" id="username" value="" placeholder="Username/ E-mail" style="box-sizing: border-box; border: none; border-bottom: 2px solid #000; border-radius: 0px; text-align: center; margin-top: 50px; margin-bottom: 50px;">
+                <input type="password" name="password" id="password" value="" placeholder="Password" style="box-sizing: border-box; border: none; border-bottom: 2px solid #000; border-radius: 0px; text-align: center; margin-bottom: 50px;">
+                <input type="checkbox" value="rememberMe" id="rememberMe">
+                <label for="rememberMe">Remember Me</label>
 
                 <a href="home.php">
                     <button style="opacity: 1; width: 205px; margin: 25px auto auto auto;">Sign In</button>
                 </a>
                 <center>
                     <a data-ajax="false" href="register.php"
-                       style="font-weight: 100; color: white; font-family: 'Poppins', sans-serif; font-size: 12px;">
+                       style="font-weight: 100; color: #000; font-family: 'Poppins', sans-serif; font-size: 12px;">
                         Don't have an account?
                     </a>
                 </center>
@@ -32,11 +34,11 @@
     </div>
 
     <!-- Footer-->
-    <div class="footer_main-no-navbar">
+    <!-- <div class="footer_main-no-navbar">
         <p>
             © 2020 Made In Sri Lanka.</br>
             Designed & Developed by Team Idea Infinite
         </p>
-    </div>
+    </div> -->
 
 </div>
