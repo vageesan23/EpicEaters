3d29b1cf1db416cdc891dc09a4fa61eb706a0da9
shangeeran <shangeeran@gmail.com>
2021-12-17 02:00:27 +0530

    added clicker page for iPad

--- ipad/pages/slashIt.php
+++ ipad/pages/slashIt.php
@@ -3,27 +3,67 @@
 <div data-role="page" id="slashit">
     <?php include '../parts/header.php' ?>
 
-    <div role="main" class="ui-content">
-        <div class="header-title" style="margin-bottom: 10px;">
+    <style>
+        .split {
+            /* height: 100%; */
+            width: 50%;
+            position: absolute;
+        }
+        .ui-content1 {
+            margin-bottom: 0px;
+        }
+    </style>
+
+    <div   class="ui-content1" style="padding: 0%;">
+        <!-- <div class="header-title" style="margin-bottom: 10px;">
             <h3>Slash It</h3>
             <img alt="page_icon" src="../../common/assets/images/icons/slashIt.png" height="35" width="35">
+        </div> -->
+        <div>
+            <img src="../../common/assets/images/clicker/RulesforiPad.jpg" 
+                style="width: 100%;">
+            <img src="../../common/assets/images/clicker/doyouknow-iPad.png" 
+                style="width: 35%; margin-left: 63%; margin-top: -250px;">
+
+            <a data-ajax="false" href="slashItPlay.php">
+                <img src="../../common/assets/images/clicker/letsplay-iPad.png" 
+                    style="width: 20%; margin-top: -120px; margin-left: 74%;">
+                </a>
+
+            <img src="../../common/assets/images/clicker/img-iPad.png" 
+                style="width: 40%;">
+            <img src="../../common/assets/images/clicker/regulation-iPad.png" 
+                style="width: 55%; ">
         </div>
 
-        <center>
+        <!-- <div>
+            <div class="split left">
+                <img src="../../common/assets/images/clicker/img-iPad.png" 
+                style="width: 80%; margin-top: 20px;">
+            </div>
+            
+            <div class="split right"> 
+                <img src="../../common/assets/images/clicker/regulation-iPad.png" 
+                style="width: 80%; ">
+            </div>
+        </div> -->
+
+        <!-- <center>
             <a data-ajax="false" href="slashItPlay.php">
-                <img src="../../common/assets/images/slashit/playNwin.png" width="340px">
+                <img src="../../common/assets/images/clicker/letsplay-iPad.png" style="width: 30%; padding-top: -40px;">
             </a>
-        </center>
-        <div style="display: flex; justify-content: space-around; padding: 40px 0px">
+        </center> -->
+        <!-- <div style="display: flex; justify-content: space-around; padding: 40px 0px">
             <img src="../../common/assets/images/slashit/slashit-banner-1.png" width="330">
             <img src="../../common/assets/images/slashit/callfriend.png" style="border-radius: 20px;">
-            <img src="../../common/assets/images/slashit/callfriend2.png" style="border-radius: 20px;">
+            <img src="../../common/assets/images/slashit/callfriend2.png" >
         </div>
         <div style="display: flex">
             <img src="../../common/assets/images/slashIt-ipad2.png"
                  style="width: 28%; margin-right: 20px; border-radius: 46px; ">
             <img src="../../common/assets/images/slashIt-ipad.png" style="width: 70%">
-        </div>
+        </div> -->
+
     </div>
         <?php include '../parts/bottomNavbar.php' ?>
         <?php include '../parts/footer.php' ?>
