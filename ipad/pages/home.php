e987eaa35a295681bfafc85e7037c0eb588691f3
vageesan23 <60152176+vageesan23@users.noreply.github.com>
2021-12-17 22:21:30 +0530

    Updated video  files

--- ipad/pages/home.php
+++ ipad/pages/home.php
@@ -5,7 +5,7 @@
 
     <!-- video -->
     <video style="object-fit: cover;" width="100%" height="240" autoplay>
-        <source src="../videos/intro.mp4" type="video/mp4">
+        <source src="../videos/intro-new-3.mp4" type="video/mp4">
         Your browser does not support the video tag.
     </video>
 
