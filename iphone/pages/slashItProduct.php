f8c110be1f8d166e2cd0e68129b27ac6d1a4c8bf
shangeeran <shangeeran@gmail.com>
2021-12-24 10:03:23 +0530

    Gamification design update

--- iphone/pages/slashItProduct.php
+++ iphone/pages/slashItProduct.php
@@ -13,32 +13,39 @@
                  style="margin: -15px 0px 0px -15px; width: 108%;">
         </center>
 
-        <div class="ui-bar ui-bar-a ui-card" style="height: unset; max-width: 70%; margin: auto;">
+        <div class="m" style ="display:flex">
+        <div class="ui-bar ui-bar-a ui-card" style="height: unset; max-width: 90%; margin-top: 50px; margin-left: 10px;">
 
-            <img class="center" src="../../common/assets/images/products/product-1.jpg" style="max-width: 120px">
+            <img class="center" src="../../common/assets/images/products/product-1.jpg" style="max-width: 320px">
+           
+        </div>
+        <p style="font-weight: bold; font-size: 10x; font-family: 'Poppins'; margin: 50px 0px 0px 20px;">
+            <?php echo $json[$id]['name'] ?? '' ?></p>
+
+        <div class="progress-wrapper" style="margin:100px 0px 0px -180px; width:250px; ">
+            <div class="progress-bar" style="background-color: #C4C4C4; margin:0px 0px 0px 50px; width: 70%;">
+                <span class="progress-bar-fill" style="width: 70%; background-color: #EF005A;
+                "></span>
+            </div>
             <div class="ui-grid-a">
-                <div class="ui-block-a" style="width: 80%">
-                    <p style="font-weight: 200; font-family: 'Poppins';"><?php echo $json[$id]['name'] ?? '' ?> </p>
+                <div class="ui-block-a" style="width: 15%; margin:40px 0px 0px 150px">
                     <div style="display: flex;">
                         <p class="card-price"
-                           style="position: relative; color: red; text-decoration: line-through;"><?php echo $json[$id]['price'] ?? '' ?></p>
+                           style="position: relative; color: #63717A; text-decoration: line-through; font-size: 15px; margin-left: -50px;">LKR <?php echo $json[$id]['price'] ?? '' ?></p>
                         <p class="card-price"
-                           style="margin-left: 20px; position: relative"><?php echo $json[$id]['price'] * 0.7 ?? '' ?></p>
+                           style="margin-left: 20px; position: relative; color: #63717A; font-size: 25px; margin: 25px 0px 0px -100px;">
+                           LKR <?php echo $json[$id]['price'] * 0.7 ?? '' ?></p>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="progress-wrapper">
-            <div class="progress-bar">
-                <span class="progress-bar-fill" style="width: 70%;"></span>
-            </div>
-            <center><h3>70%</h3></center>
         </div>
-        <div class="back-box" style="margin-bottom: 20px; margin-left: 10%; border-radius: 0px; font-size: 11px; width:  80%;">
+        
+        <div class="back-box" style="margin-bottom: 20px;margin-top: 50px; border-radius: 0px; width: 80%; margin-left: 10%;">
             <center>
                 <a href="#popupBasic" data-rel="popup" data-position-to="window"
                    data-transition="pop"><h3
-                            style="color: #EF005A;">Invite your friends to get the offer prices</h3></a>
+                            style="color: #EF005A; font-size: 12px;">Invite your friends to get the offer prices</h3></a>
             </center>
         </div>
         <div style="width: 300px; text-align: center; font-size: 12px;" data-role="popup" id="popupBasic">
