dc9f96f923ed7fd45f99d6cdfac2e745288165a0
unknown <shangar.shan33@gmail.com>
2021-12-29 21:22:41 +0530

    Signup page done for iPhone and iPad

--- ipad/pages/register.php
+++ ipad/pages/register.php
@@ -1,50 +1,117 @@
 <?php include '../parts/head.php' ?>
+<style>
+    .signup-page {
+        background: #fff;
+    }
+
+    .signup-form {
+        height: 620px;
+    }
+
+    .signout-img {
+        height: 300px;
+        margin-left: 360px;
+    }
+
+    .signup-form .ui-grid-a .ui-block-a {
+        margin-left: 40px;
+        width: 45%;
+        text-align: center; 
+    }
+
+    .signup-form .ui-grid-a .ui-block-b {
+        width: 25px;
+    }
+
+    .signup-form .ui-grid-a .ui-block-c {
+        width: 45%;
+    }
+
+    .ui-input-text {
+        border: none;
+    }
+
+    .ui-corner-all {
+        border-radius: 0px;
+    }
+
+    .ui-shadow-inset {
+        box-shadow: none;
+    }
+
+    .signup-form .ui-grid-a .form-text {
+        text-align: center;
+        border-bottom: 2px solid #000;
+        border-radius: 0px;
+        margin-bottom: 20px;
+    }
+
+    .have-account {
+        font-weight: 100;
+        color: #000 !important;
+        text-decoration: none;
+        font-family: 'Poppins', sans-serif;
+        font-size: 14px;
+        margin-top: 10px;
+    }
+
+    @media (max-width: 768px) {
+        .signup-form {
+            height: 780px;
+        }
+
+        .signout-img {
+            height: 400px;
+            margin-left: 180px;
+        }
+
+        .signup-form .ui-grid-a .form-text {
+            text-align: center;
+            border-bottom: 2px solid #000;
+            font-size: 18px;
+            border-radius: 0px;
+            margin-bottom: 30px;
+        }
+    }
+
+</style>
 <!-- This is register page-->
-<div data-role="page" id="register"
-     style='background-image: url("../../common/assets/images/christmas.jpg"); background-size: cover;'>
-    <?php include '../parts/header.php' ?>
+<div data-role="page" id="register" class="signup-page">
 
     <div role="main" class="ui-content">
 
 
-        <div class="back-box" style="padding: 50px 20px; background-color: rgba(0, 0, 0, 0.7);">
-            <div class="header-title" style="color: white">
-                <h3>Sign Up</h3>
-                <img alt="page_icon" src="../../common/assets/images/icons/registration.png" height="35" width="35">
-            </div>
-            <form style="color: #1797F3;">
-                <fieldset class="ui-grid-a">
-                    <div class="ui-block-a" style="margin-left: 50px; width: 40%">
-                        <input type="text" name="firstName" id="firstName" value="" placeholder="First Name">
-                        <input type="text" name="username" id="username" value="" placeholder="Username">
-                        <input type="password" name="password" id="password" value="" placeholder="Password">
-                    </div>
-                    <div class="ui-block-b" style="width: 20px"></div>
-                    <div class="ui-block-c" style="width: 40%">
-                        <input type="text" name="lastName" id="lastName" value="" placeholder="Last Name">
-                        <br/><br/>
-                        <input style="margin-top: -5px" type="password" name="rePassword" id="rePassword" value=""
-                               placeholder="Re-type Password">
-                    </div>
-                </fieldset><!-- /grid-a -->
-
-                <button style="opacity: 1; width: 205px; margin: 25px auto auto auto">Sign Up</button>
-                <center>
-                    <a data-ajax="false" href="logIn.php"
-                       style="font-weight: 100; color: white; font-family: 'Poppins', sans-serif; font-size: 12px;">
-                        Already have an account?
-                    </a>
-                </center>
-            </form>
+        <div class="header-title" style="display: flex;">
+            <span class="iconify" alt="page_icon" data-icon="ri:login-circle-line" data-width="50" data-height="50"></span>
+            <h3>Sign Up</h3>
         </div>
+        <form class="signup-form">
+            <img src="../../common/assets/images/signout-img.png" alt="login-img" class="signout-img">
+            <fieldset class="ui-grid-a">
+                <div class="ui-block-a" class="form-left-side">
+                    <input type="text" name="firstName" id="firstName" value="" placeholder="First Name" class="form-text">
+                    <input type="email" name="email" id="email" value="" placeholder="E-Mail" class="form-text">
+                    <input type="password" name="password" id="password" value="" placeholder="Password" class="form-text">
+                    <input type="text" name="phoneNo" id="phoneNo" value="" placeholder="Phone No" class="form-text">
+                </div>
+                <div class="ui-block-b"></div>
+                <div class="ui-block-c">
+                    <input type="text" name="lastName" id="lastName" value="" placeholder="Last Name" class="form-text">
+                    <input type="text" name="username" id="username" value="" placeholder="Username" class="form-text">
+                    <input type="password" name="rePassword" id="rePassword" value="" class="form-text" placeholder="Re-type Password">
+                </div>
+            </fieldset><!-- /grid-a -->
+
+            <a href="home.php">
+                <button style="opacity: 1; width: 205px; margin: 25px auto auto auto">Sign Up</button>
+            </a>
+            <center>
+                <a data-ajax="false" href="logIn.php" class="have-account">
+                    Already have an account?
+                </a>
+            </center>
+        </form>
 
 
-    </div>
-    <!-- Footer-->
-    <div class="footer_main-no-navbar">
-        <p>
-            © 2020 Made In Sri Lanka.</br>
-            Designed & Developed by Team Idea Infinite
-        </p>
     </div>
 </div>
